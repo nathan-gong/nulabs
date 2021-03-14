2c1e73c921c09bdbec4a8e85ba1b41f78c9f02ac
Nathan Gong <gong.n@husky.neu.edu>
2021-03-13 20:39:50 -0500

    initial commit

---
+++ create_schema.sql
@@ -0,0 +1,102 @@
+# Create Schema
+
+-- drop database nulabs;
+
+CREATE DATABASE IF NOT EXISTS nulabs;
+USE nulabs;
+
+-- Entity Tables
+
+CREATE TABLE college (
+    college_name VARCHAR(16) PRIMARY KEY,
+    building_name varchar(255) not null
+);
+
+CREATE TABLE building (
+    building_name VARCHAR(255) PRIMARY KEY,
+    street VARCHAR(255) NOT NULL,
+    college_name VARCHAR(16) NOT NULL,
+    FOREIGN KEY (college_name)
+        REFERENCES college (college_name)
+        ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE lab (
+    lab_name VARCHAR(255) PRIMARY KEY,
+    lab_description VARCHAR(1000) NOT NULL,
+    website VARCHAR(1000) NOT NULL,
+    recruiting_status TINYINT(1) NOT NULL,
+    department VARCHAR(255) NOT NULL,
+    building_name VARCHAR(255),
+    FOREIGN KEY (building_name)
+        REFERENCES building (building_name)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE lab_member (
+    username VARCHAR(255) PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    degree_level VARCHAR(64) NOT NULL,
+    lab_name VARCHAR(255) NOT NULL,
+    FOREIGN KEY (lab_name)
+        REFERENCES lab (lab_name)
+        ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+
+CREATE TABLE project (
+    title VARCHAR(256) PRIMARY KEY,
+    project_description VARCHAR(1000),
+    lab_name VARCHAR(255) NOT NULL,
+    FOREIGN KEY (lab_name)
+        REFERENCES lab (lab_name)
+        ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE publication (
+    doi VARCHAR(128) PRIMARY KEY,
+    title VARCHAR(256) NOT NULL,
+    publish_date INT NOT NULL,
+    journal VARCHAR(1000) NOT NULL
+);
+
+CREATE TABLE pi (
+    username VARCHAR(255) PRIMARY KEY,
+    lab_name VARCHAR(255) NOT NULL,
+    FOREIGN KEY (username)
+        REFERENCES lab_member (username)
+        ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (lab_name)
+        REFERENCES lab (lab_name)
+        ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+-- Relationship Tables
+
+CREATE TABLE results_in (
+    title VARCHAR(256) NOT NULL,
+    doi VARCHAR(128) NOT NULL,
+    FOREIGN KEY (title)
+        REFERENCES project (title)
+        ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (doi)
+        REFERENCES publication (doi)
+        ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE conducts (
+    username VARCHAR(255) NOT NULL,
+    title VARCHAR(256) NOT NULL,
+    lab_name VARCHAR(255) NOT NULL,
+    FOREIGN KEY (username)
+        REFERENCES lab_member (username)
+        ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (title)
+        REFERENCES project (title)
+        ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (lab_name)
+        REFERENCES lab (lab_name)
+        ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
